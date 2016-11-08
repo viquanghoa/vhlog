6c7932a421aa17bb2de2f86ecee9e16a784281e5
HoaVQ <hoavq6552@seta-asia.com.vn>
2016-11-08 11:50:33 +0700

    Finish

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
+var vLog = function () {
+  this.log = function () {
+    arguments[0] = '[INFO] ' + arguments[0];
+    console.log.apply(this, arguments);
+  };
+  this.error = function () {
+    arguments[0] = '[ERROR] ' + arguments[0];
+    console.error.apply(this, arguments);
+  };
+  this.debug = function () {
+    arguments[0] = '[DEBUG] ' + arguments[0];
+    console.log.apply(this, arguments);
+  };
+  this.trace = function () {
+    arguments[0] = '[TRACE] ' + arguments[0];
+    console.trace.apply(this, arguments);
+  };
+  this.info = function () {
+    arguments[0] = '[INFO] ' + arguments[0];
+    console.info.apply(this, arguments);
+  };
+};
+module.exports = new vLog;
